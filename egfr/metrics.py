4f6850f26004092720c5df9bfa3330c1986306c9
Huy Pham <ngochuy.yds@gmail.com>
2019-04-21 11:13:52 +0700

    Update metrics

--- egfr/metrics.py
+++ egfr/metrics.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import sklearn.metrics as metrics
-
+THRESH=0.5
 
 def auc(y_true, y_scores):
     y_true = y_true.cpu().detach().numpy()
@@ -16,7 +16,7 @@ def auc_threshold(y_true, y_scores):
     return metrics.auc(fpr, tpr)
 
 
-def get_score_obj(y_true, y_scores, thresh=0.5):
+def get_score_obj(y_true, y_scores, thresh=THRESH):
     y_true = y_true.cpu().detach().numpy()
     y_scores = (y_scores.cpu().detach().numpy() + thresh).astype(np.int16)
     return metrics.classification_report(y_true, y_scores, output_dict=True)
@@ -36,7 +36,33 @@ def class1_recall(y_true, y_scores):
     score_obj = get_score_obj(y_true, y_scores)
     return score_obj['1.0']['recall']
 
+# Metrics for benchmark
 
+def sensitivity(y_true, y_scores, thresh=THRESH):
+    y_true = y_true.cpu().detach().numpy()
+    y_scores = (y_scores.cpu().detach().numpy() + thresh).astype(np.int16)
+    tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_scores).ravel()
+    return tp / (tp + fn)
+
+
+def specificity(y_true, y_scores, thresh=THRESH):
+    y_true = y_true.cpu().detach().numpy()
+    y_scores = (y_scores.cpu().detach().numpy() + thresh).astype(np.int16)
+    tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_scores).ravel()
+    return tn / (tn + fp)
+
+
+def accuracy(y_true, y_scores, thresh=THRESH):
+    y_true = y_true.cpu().detach().numpy()
+    y_scores = (y_scores.cpu().detach().numpy() + thresh).astype(np.int16)
+    return metrics.accuracy_score(y_true, y_scores)
+
+
+
+def mcc(y_true, y_scores, thresh=THRESH):
+    y_true = y_true.cpu().detach().numpy()
+    y_scores = (y_scores.cpu().detach().numpy() + thresh).astype(np.int16)
+    return metrics.matthews_corrcoef(y_true, y_scores)
 
 
 
