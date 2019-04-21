758c2034c0157a8af9bc4010cc4239c3b5b82d56
trungle <le.hg.trung@gmail.com>
2019-04-21 13:32:13 +0700

    Combine all models into single one

--- egfr/run.py
+++ egfr/run.py
@@ -2,7 +2,7 @@ import argparse
 import torch
 import torch.nn as nn
 import tensorboard_logger
-from nets import CnnNet, DenseNet, CombinedNet, AttentionNet
+from nets import CnnNet, DenseNet, CombinedNet, AttentionNet, UnitedNet
 from torch.utils.data import dataloader
 from dataset import EGFRDataset, train_validation_split
 import torch.optim as optim
@@ -50,7 +50,8 @@ def train_validate(train_dataset,
                    n_epoch,
                    batch_size,
                    metrics,
-                   hash_code):
+                   hash_code,
+                   lr):
     train_loader = dataloader.DataLoader(dataset=train_dataset,
                                          batch_size=batch_size,
                                          collate_fn=custom_collate,
@@ -71,11 +72,11 @@ def train_validate(train_dataset,
 
     if opt_type == 'sgd':
         opt = optim.SGD(list(cnn_net.parameters()) + list(dense_net.parameters()) + list(combined_net.parameters()),
-                        lr=1e-5,
+                        lr=lr,
                         momentum=0.99)
     elif opt_type == 'adam':
         opt = optim.Adam(list(cnn_net.parameters()) + list(dense_net.parameters()) + list(combined_net.parameters()),
-                         lr=1e-5)
+                         lr=lr)
 
     for e in range(n_epoch):
         train_losses = []
@@ -113,10 +114,10 @@ def train_validate(train_dataset,
         # Validate after each epoch
         print(e, '--', 'VALIDATION ==============>')
         for i, (mord_ft, non_mord_ft, label) in enumerate(val_loader):
-            mord_ft = mord_ft.float().to(train_device)
-            non_mord_ft = non_mord_ft.view((-1, 1, 42, 150)).float().to(train_device)
-            mat_ft = non_mord_ft.narrow(2, 0, 21).view((-1, 21, 150)).float().to(train_device)
-            label = label.float().to(train_device)
+            mord_ft = mord_ft.float().to(val_device)
+            non_mord_ft = non_mord_ft.view((-1, 1, 42, 150)).float().to(val_device)
+            mat_ft = non_mord_ft.narrow(2, 0, 21).view((-1, 21, 150)).float().to(val_device)
+            label = label.float().to(val_device)
 
             with torch.no_grad():
                 non_mord_output = cnn_net(non_mord_ft)
@@ -147,6 +148,105 @@ def train_validate(train_dataset,
             tensorboard_logger.log_value('val_{}'.format(key),
                                          val_metric, e + 1)
 
+    train_metrics = {}
+    val_metrics = {}
+    for key in metrics.keys():
+        train_metrics[key] = metrics[key](train_labels, train_outputs)
+        val_metrics[key] = metrics[key](val_labels, val_outputs)
+
+    return train_metrics, val_metrics
+
+
+def train_validate_united(train_dataset,
+                          val_dataset,
+                          train_device,
+                          val_device,
+                          opt_type,
+                          n_epoch,
+                          batch_size,
+                          metrics,
+                          hash_code,
+                          lr):
+    train_loader = dataloader.DataLoader(dataset=train_dataset,
+                                         batch_size=batch_size,
+                                         collate_fn=custom_collate,
+                                         shuffle=True)
+
+    val_loader = dataloader.DataLoader(dataset=val_dataset,
+                                       batch_size=batch_size,
+                                       collate_fn=custom_collate,
+                                       shuffle=True)
+
+    tensorboard_logger.configure('logs/' + hash_code)
+
+    criterion = nn.BCELoss()
+    united_net = UnitedNet(dense_dim=train_dataset.get_dim('mord'), use_mat=True).to(train_device)
+
+    if opt_type == 'sgd':
+        opt = optim.SGD(united_net.parameters(),
+                        lr=lr,
+                        momentum=0.99)
+    elif opt_type == 'adam':
+        opt = optim.Adam(united_net.parameters(),
+                         lr=lr)
+
+    for e in range(n_epoch):
+        train_losses = []
+        val_losses = []
+        train_outputs = []
+        val_outputs = []
+        train_labels = []
+        val_labels = []
+        print(e, '--', 'TRAINING ==============>')
+        for i, (mord_ft, non_mord_ft, label) in enumerate(train_loader):
+            mord_ft = mord_ft.float().to(train_device)
+            non_mord_ft = non_mord_ft.view((-1, 1, 42, 150)).float().to(train_device)
+            mat_ft = non_mord_ft.narrow(2, 0, 21).view((-1, 21, 150)).float().to(train_device)
+            label = label.float().to(train_device)
+
+            # Forward
+            opt.zero_grad()
+            outputs = united_net(non_mord_ft, mord_ft, mat_ft)
+
+            loss = criterion(outputs, label)
+            train_losses.append(float(loss.item()))
+            train_outputs.extend(outputs)
+            train_labels.extend(label)
+
+            # Parameters update
+            loss.backward()
+            opt.step()
+
+        # Validate after each epoch
+        print(e, '--', 'VALIDATION ==============>')
+        for i, (mord_ft, non_mord_ft, label) in enumerate(val_loader):
+            mord_ft = mord_ft.float().to(val_device)
+            non_mord_ft = non_mord_ft.view((-1, 1, 42, 150)).float().to(val_device)
+            mat_ft = non_mord_ft.narrow(2, 0, 21).view((-1, 21, 150)).float().to(val_device)
+            label = label.float().to(val_device)
+
+            with torch.no_grad():
+                outputs = united_net(non_mord_ft, mord_ft, mat_ft)
+
+                loss = criterion(outputs, label)
+                val_losses.append(float(loss.item()))
+                val_outputs.extend(outputs)
+                val_labels.extend(label)
+
+        train_outputs = torch.stack(train_outputs)
+        val_outputs = torch.stack(val_outputs)
+        train_labels = torch.stack(train_labels)
+        val_labels = torch.stack(val_labels)
+        tensorboard_logger.log_value('train_loss', sum(train_losses) / len(train_losses), e + 1)
+        tensorboard_logger.log_value('val_loss', sum(val_losses) / len(val_losses), e + 1)
+
+        for key in metrics.keys():
+            train_metric = metrics[key](train_labels, train_outputs)
+            val_metric = metrics[key](val_labels, val_outputs)
+            tensorboard_logger.log_value('train_{}'.format(key),
+                                         train_metric, e + 1)
+            tensorboard_logger.log_value('val_{}'.format(key),
+                                         val_metric, e + 1)
 
     train_metrics = {}
     val_metrics = {}
@@ -156,6 +256,7 @@ def train_validate(train_dataset,
 
     return train_metrics, val_metrics
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', help='Input dataset', dest='dataset', default='data/egfr_10_full_ft_pd_lines.json')
@@ -164,6 +265,7 @@ def main():
     parser.add_argument('-o', '--opt', help='Optimizer adam or sgd', dest='opt', default='adam')
     parser.add_argument('-g', '--gpu', help='Use GPU or Not?', action='store_true')
     parser.add_argument('-c', '--hashcode', help='Hashcode for tf.events', dest='hashcode', default='TEST')
+    parser.add_argument('-l', '--lr', help='Learning rate', dest='lr', default=1e-5)
     args = parser.parse_args()
 
     train_data, val_data = train_validation_split(args.dataset)
@@ -182,8 +284,9 @@ def main():
                    args.opt,
                    int(args.epochs),
                    int(args.batchsize),
-                   {'sensitivity': b_sensitivity, 'specificity': b_specificity, 'accuracy': b_accuracy, 'mcc': b_mcc, 'roc': b_roc},
-                   args.hashcode)
+                   {'sensitivity': sensitivity, 'specificity': specificity, 'accuracy': accuracy, 'mcc': mcc, 'auc': auc},
+                   args.hashcode,
+                   args.lr)
 
 
 if __name__ == '__main__':
