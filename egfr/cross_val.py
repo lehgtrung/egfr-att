4d680af177471700c7001f7cef6bd75cca6ac9e0
Huy Pham <ngochuy.yds@gmail.com>
2019-06-08 12:59:32 +0700

    Update Cross Validation code

--- egfr/cross_val.py
+++ egfr/cross_val.py
@@ -4,16 +4,22 @@ import torch.nn as nn
 import tensorboard_logger
 from nets import UnitedNet
 from torch.utils.data import dataloader
-from dataset import EGFRDataset, train_cross_validation_split
+from dataset import EGFRDataset, train_validation_split, train_cross_validation_split
 import torch.optim as optim
 from metrics import *
 import utils
-
+import matplotlib.pyplot as plt
+import os
+from sklearn.metrics import precision_recall_curve
+plt.switch_backend('agg')
+import pandas as pd
 
 def train_validate_united(train_dataset,
                           val_dataset,
                           train_device,
                           val_device,
+                          use_mat,
+                          use_mord,
                           opt_type,
                           n_epoch,
                           batch_size,
@@ -34,7 +40,8 @@ def train_validate_united(train_dataset,
     # tensorboard_logger.configure('logs/' + hash_code)
 
     criterion = nn.BCELoss()
-    united_net = UnitedNet(dense_dim=train_dataset.get_dim('mord'), use_mat=True).to(train_device)
+    united_net = UnitedNet(dense_dim=train_dataset.get_dim('mord'),
+                           use_mat=use_mat, use_mord=use_mord).to(train_device)
 
     if opt_type == 'sgd':
         opt = optim.SGD(united_net.parameters(),
@@ -54,7 +61,7 @@ def train_validate_united(train_dataset,
         val_outputs = []
         train_labels = []
         val_labels = []
-        print('FOLD', fold, '-- EPOCH', e+1, '--', 'TRAINING')
+        print('FOLD', fold, '-- EPOCH', e, '-- TRAINING')
         for i, (mord_ft, non_mord_ft, label) in enumerate(train_loader):
             united_net.train()
             mord_ft = mord_ft.float().to(train_device)
@@ -76,7 +83,7 @@ def train_validate_united(train_dataset,
             opt.step()
 
         # Validate after each epoch
-        print('FOLD', fold, '-- EPOCH', e+1, '--', 'VALIDATION')
+        print('FOLD', fold, 'EPOCH', e, '--', 'VALIDATING')
         for i, (mord_ft, non_mord_ft, label) in enumerate(val_loader):
             united_net.eval()
             mord_ft = mord_ft.float().to(val_device)
@@ -86,6 +93,7 @@ def train_validate_united(train_dataset,
 
             with torch.no_grad():
                 outputs = united_net(non_mord_ft, mord_ft, mat_ft)
+                # print(outputs[-1, -20:])
 
                 loss = criterion(outputs, label)
                 val_losses.append(float(loss.item()))
@@ -98,15 +106,13 @@ def train_validate_united(train_dataset,
         val_labels = torch.stack(val_labels)
         tensorboard_logger.log_value('train_loss', sum(train_losses) / len(train_losses), e + 1)
         tensorboard_logger.log_value('val_loss', sum(val_losses) / len(val_losses), e + 1)
-        if (e+1) % 10 == 0:
-            print('{"metric": "train_loss", "value": %f, "epoch": %d}' % (sum(train_losses) / len(train_losses), e + 1))
-            print('{"metric": "val_loss", "value": %f, "epoch": %d}' % (sum(val_losses) / len(val_losses), e + 1))
+        print('{"metric": "train_loss", "value": %f, "epoch": %d}' % (sum(train_losses) / len(train_losses), e + 1))
+        print('{"metric": "val_loss", "value": %f, "epoch": %d}' % (sum(val_losses) / len(val_losses), e + 1))
         for key in metrics.keys():
             train_metric = metrics[key](train_labels, train_outputs)
             val_metric = metrics[key](val_labels, val_outputs)
-            if ((e+1) % 10) == 0:
-                print('{"metric": "%s", "value": %f, "epoch": %d}' % ('train_' + key, train_metric, e + 1))
-                print('{"metric": "%s", "value": %f, "epoch": %d}' % ('val_' + key, val_metric, e + 1))
+            print('{"metric": "%s", "value": %f, "epoch": %d}' % ('train_' + key, train_metric, e + 1))
+            print('{"metric": "%s", "value": %f, "epoch": %d}' % ('val_' + key, val_metric, e + 1))
             tensorboard_logger.log_value('train_{}'.format(key),
                                          train_metric, e + 1)
             tensorboard_logger.log_value('val_{}'.format(key),
@@ -117,10 +123,10 @@ def train_validate_united(train_dataset,
             min_loss_idx = e
             print(min_loss_idx)
             min_loss = loss_epoch
-            utils.save_model(united_net, "data/trained_models", hash_code + '_' + str(fold + 1))
+            utils.save_model(united_net, "data/trained_models", hash_code + "_" + str(fold))
         else:
             early_stop_count += 1
-            if early_stop_count > 20:
+            if early_stop_count > 30:
                 print('Traning can not improve from epoch {}\tBest loss: {}'.format(e, min_loss))
                 break
 
@@ -133,12 +139,13 @@ def train_validate_united(train_dataset,
     return train_metrics, val_metrics
 
 
-def predict(dataset, model_path, device='cpu'):
+def predict(dataset, model_path, device='cpu', use_mat=True, use_mord=True):
     loader = dataloader.DataLoader(dataset=dataset,
                                    batch_size=128,
                                    collate_fn=utils.custom_collate,
                                    shuffle=False)
-    united_net = UnitedNet(dense_dim=dataset.get_dim('mord'), use_mat=True).to(device)
+    united_net = UnitedNet(dense_dim=dataset.get_dim('mord'),
+                           use_mat=use_mat, use_mord=use_mord).to(device)
     united_net.load_state_dict(torch.load(model_path, map_location=device))
     # EVAL_MODE
     united_net.eval()
@@ -153,9 +160,35 @@ def predict(dataset, model_path, device='cpu'):
             probas.append(proba)
     print('Forward done !!!')
     probas = np.concatenate(probas)
-    # probas = probas.squeeze()
     return probas
 
+
+def plot_roc_curve(y_true, y_pred, hashcode=''):
+
+    if not os.path.exists('vis/'):
+        os.makedirs('vis/')
+
+    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred, pos_label=1)
+    auc_roc = metrics.roc_auc_score(y_true, y_pred)
+    print('AUC: {:4f}'.format(auc_roc))
+    plt.plot(fpr, tpr)
+    plt.savefig('vis/ROC_{}'.format(hashcode + '.png'))
+    plt.clf()  # Clear figure
+
+
+def plot_precision_recall(y_true, y_pred, hashcode=''):
+
+    if not os.path.exists('vis/'):
+        os.makedirs('vis/')
+
+    precisions, recalls, thresholds = precision_recall_curve(y_true, y_pred)
+    plt.plot(thresholds, precisions[:-1], label="Precision")
+    plt.plot(thresholds, recalls[:-1], label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="upper left")
+    plt.ylim([0, 1])
+    plt.savefig('vis/PR_{}'.format(hashcode + '.png'))
+    plt.clf()  # Clear figure
 
 def main():
     parser = argparse.ArgumentParser()
@@ -169,10 +202,11 @@ def main():
     parser.add_argument('-l', '--lr', help='Learning rate', dest='lr', default=1e-5, type=float)
     parser.add_argument('-k', '--mode', help='Train or predict ?', dest='mode', default='train', type=str)
     parser.add_argument('-m', '--model_path', help='Trained model path', dest='model_path', type=str)
-
+    parser.add_argument('-uma', '--use_mat', help='Use mat feature or not', dest='use_mat', type=int, required=True)
+    parser.add_argument('-umo', '--use_mord', help='Use mat feature or not', dest='use_mord', type=int, required=True)
     args = parser.parse_args()
 
-    if args.gpu:
+    if torch.cuda.is_available():
         train_device = 'cuda'
         val_device = 'cuda'
     else:
@@ -181,62 +215,56 @@ def main():
 
     tensorboard_logger.configure('logs/' + args.hashcode)
 
-    train_metrics_cv = []
-    val_metrics_cv = []
-    best_cv = []
-    metrics_dict = {'sensitivity': sensitivity,
-                    'specificity': specificity,
-                    'accuracy': accuracy,
-                    'mcc': mcc, 'auc': auc}
     metrics_cv_dict = {'sensitivity': sensitivity_cv,
                        'specificity': specificity_cv,
                        'accuracy': accuracy_cv,
                        'mcc': mcc_cv,
                        'auc': auc_cv}
 
-    for fold, (train_data, val_data) in enumerate(train_cross_validation_split(args.dataset)):
+    train_cv_metrics = []
+    val_cv_metrics = []
+    best_cv = []
+    fold = 0
+    for train_data, val_data in train_cross_validation_split(args.dataset):
+        fold += 1
         train_dataset = EGFRDataset(train_data)
         val_dataset = EGFRDataset(val_data)
-
+        # print('Train', len(train_dataset))
+        # print('Val', len(val_dataset))
         train_metrics, val_metrics = train_validate_united(train_dataset,
-                                                           val_dataset,
-                                                           train_device,
-                                                           val_device,
-                                                           args.opt,
-                                                           int(args.epochs),
-                                                           int(args.batchsize),
-                                                           {'sensitivity': sensitivity, 'specificity': specificity,
-                                                            'accuracy': accuracy, 'mcc': mcc, 'auc': auc},
-                                                           args.hashcode,
-                                                           args.lr,
+                                                          val_dataset,
+                                                          train_device,
+                                                          val_device,
+                                                          args.use_mat == 1,
+                                                          args.use_mord == 1,
+                                                          args.opt,
+                                                          int(args.epochs),
+                                                          int(args.batchsize),
+                                                          {'sensitivity': sensitivity, 'specificity': specificity,
+                                                           'accuracy': accuracy, 'mcc': mcc, 'auc': auc},
+                                                          args.hashcode,
+                                                          args.lr,
                                                            fold)
+        train_cv_metrics.append(train_metrics)
+        val_cv_metrics.append(val_metrics)
 
-        train_metrics_cv.append(train_metrics)
-        val_metrics_cv.append(val_metrics)
-        filename = "data/trained_models/model_" + args.hashcode + '_' + str(fold + 1) + "_BEST"
-
-        y_pred = predict(val_dataset, filename)
+        # VALIDATE in BEST MODEL
+        filename = "data/trained_models/model_" + args.hashcode + '_' + str(fold) + "_BEST"
+        y_pred = predict(val_dataset, filename, use_mat=args.use_mat, use_mord=args.use_mord)
         y_true = val_dataset.label
-        bestcv = []
-        for m in metrics_cv_dict.values():
-            bestcv.append(m(y_true, y_pred))
+        bestcv = {}
+        for metric, metric_fun in metrics_cv_dict.items():
+            bestcv[metric] = metric_fun(y_true, y_pred)
 
-        print('val_metrics', val_metrics)
-        print('Test', bestcv)
         best_cv.append(bestcv)
 
-    train_metrics_cv = np.round(np.array([list(d.values()) for d in train_metrics_cv]).mean(axis=0), decimals=4)
-    val_metrics_cv = np.round(np.array([list(d.values()) for d in val_metrics_cv]).mean(axis=0), decimals=4)
-    best_cv = np.round(np.array([d for d in best_cv]).mean(axis=0), decimals=4)
-    print(train_metrics_cv)
-    print(val_metrics_cv)
-    print(best_cv)
-    row_format = "{:>12}" * (len(metrics_dict.keys()) + 1)
-    print(row_format.format("", *(metrics_dict.keys())))
-    print(row_format.format("Train", *train_metrics_cv))
-    print(row_format.format("Val", *val_metrics_cv))
-    print(row_format.format("Load", *best_cv))
-
+    train_metrics_df = pd.DataFrame(train_cv_metrics)
+    val_metrics_df = pd.DataFrame(val_cv_metrics)
+    best_cv_df = pd.DataFrame(best_cv)
+    cv_metrics_df = pd.DataFrame([train_metrics_df.mean(), val_metrics_df.mean(), best_cv_df.mean()], index = ['train', 'val', 'load'])
+    print(cv_metrics_df[['sensitivity', 'specificity', 'accuracy', 'mcc', 'auc']])
 
 if __name__ == '__main__':
     main()
+
+
